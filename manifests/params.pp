a9e325d122f6abcd0d33007b5310cf61c4385890
Cam Parry <cam.parry@gmail.com>
2014-06-17 15:34:24 +0100

    More sensible default params

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,20 +8,20 @@ class serf::params{
   $log_dir          = '/var/log/serf'
   $encrypt          = ''
   $log_level        = 'info'
-  $log_file         = '/var/log/serf.log'
+  $log_file         = "$log_dir/serf.log"
   $node             = $::fqdn
   $protocol         = $::serf::params::protocol
   $role             = $::serf::params::role
   $rpc_addr         = "${bind}:7373"
   $install_path     = '/usr/local/bin/'
   $install_method   = 'url'
-  $sample_handler   = true
-  $handler_home     = "/opt/serf"
+  $sample_handler   = false
+  $handler_home     = "{config_dir}/handlers"
   $package_name     = 'serf'
   $package_ensure   = 'present'
 
   $event_handler    = [
-    '/opt/serf/handler.sh'
+    '/etc/serf/handlers/handler.sh'
   ]
   $join             = [
     '127.0.0.1'
